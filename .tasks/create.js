0ff08d4572b38f359e486d4fc5aebcf900aa8f80
Iván Barcia <ivan@barcia.dev>
2019-04-19 23:02:55 +0200

    Update notes UID system

---
+++ .tasks/create.js
@@ -0,0 +1,36 @@
+const config = require('./paths.json');
+const fs = require('fs');
+const { DateTime } = require("luxon");
+const generate = require('nanoid/generate');
+const alphabet = "23456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnpqrstwxyz";
+
+
+module.exports = {
+	note: function(done) {
+		const uid = generate(alphabet, 8);
+		const date = DateTime.local();
+		const frontMatter = `---
+date: ${date.toISO()}
+---`;
+
+		fs.writeFile(`${config.notes}${date.toISODate()}-${uid}.md`, frontMatter, function (err) {
+			if (err) throw err;
+			console.log(`Note with UID: ${uid} was created successfully.`);
+		});
+		done();
+	},
+	article: function(done) {
+		const date = DateTime.local();
+		const frontMatter = `---
+title:
+date: ${date.toISO()}
+tags: [""]
+---`;
+
+	fs.writeFile(`${config.articles}${date.toISODate()}-TITLE.md`, frontMatter, function (err) {
+		if (err) throw err;
+		console.log(`Article was created successfully.`);
+	});
+	done();
+	}
+}
--- gulpfile.js
+++ gulpfile.js
@@ -6,8 +6,11 @@ const gulp = require('gulp');
 const css = require('./.tasks/css');
 const img = require('./.tasks/images');
 const js = require('./.tasks/javascript');
+const create = require('./.tasks/create');
 const watch = require('./.tasks/watch');
 
 // Exports
 exports.watch = watch.assets;
 exports.build = gulp.parallel(css.prod, js.prod, img);
+exports.create_note = create.note;
+exports.create_article = create.article;

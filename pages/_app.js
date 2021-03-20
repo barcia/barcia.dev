86b0384cf58c3de031cc994175a9e7f223984565
Iván Barcia <ivanbarcia@icloud.com>
2021-03-20 01:29:01 +0100

    Add plausible analytics

--- pages/_app.js
+++ pages/_app.js
@@ -9,6 +9,7 @@ function MyApp({ Component, pageProps }) {
         <meta name="description" content="Deseñador web e desenvolvedor. Interesado en estándares web, CSS, JavaScript, deseño, UX, e desenvolvemento de produtos dixitais en xeral." />
         <meta name="theme-color" content="#000000" />
         <link rel="icon" href="/favicon.ico" />
+        <script async defer data-domain="barcia.dev" src="https://plausible.io/js/plausible.js"></script>
       </Head>
       <Component {...pageProps} />
     </>

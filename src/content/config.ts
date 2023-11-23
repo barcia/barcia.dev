b8c4aeb0ccb90a6b3646b0a274b05b3f7d0a25a8
Iván Barcia <ivanbarcia@icloud.com>
2023-11-23 21:46:44 +0100

    First version

--- src/content/config.ts
+++ src/content/config.ts
@@ -2,13 +2,12 @@ import { defineCollection, z } from 'astro:content'
 
 // 2. Define your collection(s)
 const workCollections = defineCollection({
-  type: 'data',
+  type: 'content',
   schema: ({ image }) => z.object({
     name: z.string(),
     logo: image(),
     date_start: z.number(),
     date_end: z.union([z.number(), z.boolean()]),
-    description: z.string()
   }),
 });
 

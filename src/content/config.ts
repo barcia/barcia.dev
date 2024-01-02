e372d462eec717bc5af7c206f8b30489fb90bacc
Iván Barcia <ivanbarcia@icloud.com>
2024-01-02 20:27:56 +0100

    Add newsletter page

--- src/content/config.ts
+++ src/content/config.ts
@@ -20,12 +20,12 @@ const speakingCollection = defineCollection({
   }),
 });
 
-const issuesCollection = defineCollection({
+const newsletterCollection = defineCollection({
   type: 'content',
 });
 
 export const collections = {
   'work': workCollection,
   'speaking': speakingCollection,
-  'issues': issuesCollection,
+  'newsletter': newsletterCollection,
 };

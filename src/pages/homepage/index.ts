33b68e680b8d9757bdf7c929f3e0b02cf4f3435c
Iván Barcia <ivan@barcia.dev>
2025-03-31 23:59:29 +0200

    add some sections

---
+++ src/pages/homepage/index.ts
@@ -0,0 +1,4 @@
+export { default as About } from "./about.astro";
+export { default as Contact } from "./contact.astro";
+export { default as Companies } from "./companies.astro";
+export { default as Work } from "./work.astro";
--- src/pages/now.md
+++
@@ -1,6 +0,0 @@
----
-layout: ../components/layouts/page.astro
-title: "Now"
----
-
-This is my now page.

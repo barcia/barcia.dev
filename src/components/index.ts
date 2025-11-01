699eeff09564249d14df3f73d25e168e7c167d9e
Iván Barcia <ivan@barcia.dev>
2025-11-01 22:29:09 +0100

    Update and refactor some old code

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,1 @@
-export { default as Footer } from "./Footer.astro";
 export { default as Icon } from "./Icon.astro";
-export { default as Navbar } from "./Navbar.astro";
-export { default as Section } from "./Section.astro";
-export { default as Share } from "./Share.astro";

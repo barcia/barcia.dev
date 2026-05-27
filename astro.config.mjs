cb98dd903cc35826512431a84bc8a8acca4f4881
Iván Barcia <ivan@barcia.dev>
2026-05-27 17:20:46 +0200

    Change font to Geist Mono

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,16 @@
 import tailwindcss from "@tailwindcss/vite";
-import { defineConfig } from "astro/config";
+import { defineConfig, fontProviders } from "astro/config";
 
 export default defineConfig({
 	site: "https://barcia.dev",
 	vite: {
 		plugins: [tailwindcss()],
 	},
+	fonts: [{
+		provider: fontProviders.fontsource(),
+		name: "Geist Mono",
+		cssVariable: "--font-geist-mono",
+	}],
 	markdown: {
 		shikiConfig: {
 			themes: {
